3a776441e761c02107d8cbc588f3c481aa1c6336
Anthony Grimes <i@raynes.me>
2011-07-19 17:22:02 -0500

    Add cake-marginalia

--- project.clj
+++ project.clj
@@ -1,3 +1,5 @@
 (defproject retro "0.6.0-alpha1"
   :description "A protocol for revisioned transactions in Clojure."
-  :dependencies [[clojure "1.2.0"]])
+  :dependencies [[clojure "1.2.0"]]
+  :dev-dependencies [[org.clojars.flatland/cake-marginalia "0.6.1"]]
+  :tasks [cake-marginalia.tasks])
