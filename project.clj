14316541cb5b1b7744fb2d0b53db38f4c71017e8
Alan Malloy <alan@malloys.org>
2011-10-06 15:30:53 -0700

    Bump version

--- project.clj
+++ project.clj
@@ -1,5 +1,6 @@
-(defproject retro "0.6.0-alpha1"
+(defproject retro "0.6.0-alpha2"
   :description "A protocol for revisioned transactions in Clojure."
   :dependencies [[clojure "1.2.0"]]
-  :dev-dependencies [[org.clojars.flatland/cake-marginalia "0.6.1"]]
-  :tasks [cake-marginalia.tasks])
+;  :dev-dependencies [[org.clojars.flatland/cake-marginalia "0.6.1"]]
+;  :tasks [cake-marginalia.tasks]
+  )
