5e7c7145458a9892840e113edddebc9b1f17ba88
Justin Balthrop <git@justinbalthrop.com>
2011-10-09 08:29:02 -0700

    0.5.2

--- project.clj
+++ project.clj
@@ -1,5 +1,3 @@
-(defproject retro "0.6.0-alpha1"
+(defproject retro "0.5.2"
   :description "A protocol for revisioned transactions in Clojure."
-  :dependencies [[clojure "1.2.0"]]
-  :dev-dependencies [[org.clojars.flatland/cake-marginalia "0.6.1"]]
-  :tasks [cake-marginalia.tasks])
+  :dependencies [[clojure "1.2.0"]])
