0e1b1d5a19494a1b504d38875f2a6f613ac5c7f1
Alan Malloy <alan@malloys.org>
2011-10-11 13:06:31 -0700

    Add test for in-transaction visibility rules

--- test/retro/core_test.clj
+++ test/retro/core_test.clj
@@ -30,9 +30,12 @@
 (defn make [init-data]
   (RevisionSeq. (ref init-data) []))
 
+(defn revisioned-update [rev-seq f & args]
+  (apply alter (:data rev-seq)
+         update-in [(inc (current-revision rev-seq))] f args))
+
 (defn revisioned-set [rev-seq data]
-  (alter (:data rev-seq)
-         assoc (inc (current-revision rev-seq)) data))
+  (revisioned-update rev-seq (constantly data)))
 
 (defn hard-set [rev-seq data]
   (ref-set (:data rev-seq) data))
@@ -60,3 +63,13 @@
       (enqueue obj #(revisioned-set % 100)))
     (is (= 20 (get-data obj)))
     (is (= 2 (max-rev obj)))))
+
+(deftest test-visibility
+  (binding [*print-meta* true]
+    (let [obj (at-revision (make [10 20]) 1)]
+      (dotxn obj
+        (let [obj (enqueue obj #(revisioned-set % 30))]
+          (is (thrown? Exception ;; shouldn't actually be written yet
+                       (get-data (at-revision obj 2))))
+          (enqueue obj #(revisioned-update % inc))))
+      (is (= 31 (get-data (at-revision obj nil)))))))
