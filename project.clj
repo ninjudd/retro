01405d230287092b3de48627a476f00be6f7b100
Anthony Grimes <i@raynes.me>
2011-12-09 15:57:22 -0600

    (inc alpha)

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject retro "0.6.0-alpha3"
+(defproject retro "0.6.0-alpha4"
   :description "A protocol for revisioned transactions in Clojure."
   :dependencies [[clojure "1.3.0"]
                  [useful "0.7.4-alpha4"]])
