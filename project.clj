efda552e92991047547db06696380dedb8097e23
Alan Malloy <alan@malloys.org>
2011-12-09 15:57:42 -0800

    Alpha 6

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject retro "0.6.0-alpha5"
+(defproject retro "0.6.0-alpha6"
   :description "A protocol for revisioned transactions in Clojure."
   :dependencies [[clojure "1.3.0"]
                  [useful "0.7.4-alpha4"]])
