4d66265ceb3b0486faf06fd557822a7085b2d902
Anthony Grimes <i@raynes.me>
2012-01-06 15:33:30 -0600

    Update to Clojure 1.3 and inc version.

--- project.clj
+++ project.clj
@@ -1,3 +1,3 @@
-(defproject retro "0.5.3"
+(defproject retro "0.5.4"
   :description "A protocol for revisioned transactions in Clojure."
-  :dependencies [[clojure "1.2.0"]])
+  :dependencies [[clojure "1.3.0"]])
