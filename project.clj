d48030d80ce42a515eef31ecf8e768fb529ab670
Alan Malloy <alan@malloys.org>
2012-07-20 12:42:40 -0700

    alpha1
    A released version of the new transaction model to play with.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject retro "0.6.0"
+(defproject retro "0.7.0-alpha1"
   :description "A protocol for revisioned transactions in Clojure."
   :dependencies [[org.clojure/clojure "1.3.0"]
                  [useful "0.7.5"]])
