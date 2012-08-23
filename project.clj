21831dbda27577d659c6b1fa7d33f363bff775c2
Anthony Grimes <i@raynes.me>
2012-08-22 19:54:31 -0500

    Add url and license keys.

--- project.clj
+++ project.clj
@@ -1,4 +1,7 @@
 (defproject retro "0.7.0"
   :description "A protocol for revisioned transactions in Clojure."
+  :url "https://github.com/flatland/retro"
+  :license  {:name "Eclipse Public License - v 1.0"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.3.0"]
-                 [useful "0.7.5"]])
+                 [useful "0.8.4"]])
