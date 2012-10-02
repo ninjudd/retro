a275be18e3a9c8203c68f7a8e7f86f47ed578858
Alan Malloy <alan@malloys.org>
2012-10-02 16:03:30 -0700

    0.7.1

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject retro "0.7.0"
+(defproject retro "0.7.1"
   :description "A protocol for revisioned transactions in Clojure."
   :url "https://github.com/flatland/retro"
   :license  {:name "Eclipse Public License - v 1.0"
