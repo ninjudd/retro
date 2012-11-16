0c700e968c026fb18fb90536abb3affbcf666772
Justin Balthrop <git@justinbalthrop.com>
2012-11-16 10:57:42 -0800

    0.8.0

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
-(defproject org.flatland/retro "0.7.2-SNAPSHOT"
+(defproject org.flatland/retro "0.8.0"
   :description "A protocol for revisioned transactions in Clojure."
   :url "https://github.com/flatland/retro"
   :license  {:name "Eclipse Public License - v 1.0"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.3.0"]
-                 [org.flatland/useful "0.8.9-SNAPSHOT"]])
+                 [org.flatland/useful "0.9.0"]])
